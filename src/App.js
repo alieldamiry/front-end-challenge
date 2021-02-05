eccf61ed2ffd8da9b6f557aaf9ac9e7bfde7e93f
alieldamiry <alieldamiry@gmail.com>
2021-02-05 18:16:24 +0200

    fetched repositories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.scss';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Repo from './components/Repo/Repo';
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=1')
+      .then((res) => {
+        setRepos([...res.data.items]);
+      });
+  }, []);
+
+  console.log(repos);
   return (
     <div className="container">
       <Repo />
